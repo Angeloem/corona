4e46918ded9182f119afa302900fef5936b81e86
Angeloem <esanga530@gmail.com>
2020-04-30 22:54:09 +0300

    Added tiles which do give fast information of the disease

---
+++ src/components/tile.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import './styles/tiles.scss';
+
+export const Tile = () => {
+    return (
+        <div className={'tile'}>
+            <div className={"head"}>
+                'number'
+            </div>
+            <div className="stat">
+                 'head'
+            </div>
+        </div>
+    )
+};
+
+export const Tile2 = () => {
+    return (
+        <div className={'tile2'}>
+            <div className={"head"}>
+                'number'
+            </div>
+            <div className="stat">
+                 'head'
+            </div>
+        </div>
+    )
+};
+
+export const Tile3 = () => {
+    return (
+        <div className={'tile3'}>
+            <div className={"head"}>
+                'number'
+            </div>
+            <div className="stat">
+                 'head'
+            </div>
+        </div>
+    )
+};
+
+export const Tile4 = () => {
+    return (
+        <div className={'tile4'}>
+            <div className={"head"}>
+                'number'
+            </div>
+            <div className="stat">
+                 'head'
+            </div>
+        </div>
+    )
+};
